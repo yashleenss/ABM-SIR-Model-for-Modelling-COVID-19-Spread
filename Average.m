ce44d9c8352dd1d619bf91ab880ccff725d9ad26
yashleenss <136642563+yashleenss@users.noreply.github.com>
2025-06-24 19:25:50 -0700

    Average.m

--- Average.m
+++ Average.m
@@ -1,24 +1,12 @@
-function Avg = Average(P_H0)
+%--------------------------------------------------------%
+% Calculating AVERAGE infection probability per age group 
+%--------------------------------------------------------%
 
-  N = 5000;
-% Age Group distribution (percentages)
-  elderly_percentage = 0.17;     % 17% are elderly
-  adult_percentage = 0.63;       % 63% are adults
-  kids_percentage = 0.20;        % 20% are kids
-
-% Number of individuals in each group
-  n_elderly = round(N * elderly_percentage);
-  n_adults = round(N * adult_percentage);
-  n_kids = N - n_elderly - n_adults; % Remaining are kids
+function Avg = Average(age_group, P_H0)
     
-% Separate the P_H vector into 3 age groups
-  kids = P_H0(1:n_kids, :); 
-  adults = P_H0(n_kids+1:n_kids+n_adults, :); 
-  elderly = P_H0(n_kids+n_adults+1:end, :); 
-    
-% Calculate the average summation per day for each group
-  Avg.kids = sum(kids, 1) / n_kids;
-  Avg.adults = sum(adults, 1) / n_adults;
-  Avg.elderly = sum(elderly, 1) / n_elderly;
+        % Calculate the average summation per day for each group
+          Avg.elderly = mean(P_H0(age_group == 1, :), 1);
+          Avg.adults = mean(P_H0(age_group == 2, :), 1);
+          Avg.kids = mean(P_H0(age_group == 3, :), 1);
 
 end
