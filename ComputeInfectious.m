1c93741740c85e3bde6e383f0612065db7a31735
yashleenss <136642563+yashleenss@users.noreply.github.com>
2025-06-24 19:31:14 -0700

    ComputeInfectious.m

--- ComputeInfectious.m
+++ ComputeInfectious.m
@@ -1,27 +1,30 @@
+%-----------------------------------------------------%
+% Calculating the initial infection period and variant
+%-----------------------------------------------------%
+
 function infection = ComputeInfectious(tI, t, variant)
 
-% Variant type and calculate the infectious period
-  switch variant
-  case 'Alpha and Delta'
+      % Infectious period according to variant type
+        switch variant
 
-    % Use element-wise logical operators RHS and LHS are vectors
-    % Infection period for an individual is between 4th and 7th day
-      if ((t-tI) >= 4) && ((t-tI) <= 7)
-          infection = 1;
-      else
-          infection = 0;
-      end 
+      % Use element-wise logical operators RHS and LHS are vectors
+      % Infection period for an individual is between 4th and 7th day
+        case 'Alpha and Delta'
+             if ((t-tI) >= 4) && ((t-tI) <= 7)
+                      infection = 1;
+             else
+                      infection = 0;
+             end 
         
- 
-  % Infection period for an individual is between 2nd and 5th day
-    case 'Omicron'
-    if ((t-tI) >= 2) && ((t-tI) <= 5)
-          infection = 1;
-      else
-          infection = 0;
-    end  
+      % Infection period for an individual is between 2nd and 5th day
+        case 'Omicron'
+             if ((t-tI) >= 2) && ((t-tI) <= 5)
+                      infection = 1;
+            else
+                      infection = 0;
+            end  
 
-    otherwise
-          infection = 0;
-  end
+        otherwise
+                      infection = 0;
+        end
 end
